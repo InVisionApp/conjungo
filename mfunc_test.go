2c7489e0d8b6a821dca9dc325210381b3c3820d5
Tatsuro Alpert <tatsuro@invisionapp.com>
2017-02-06 10:05:31 -0800

    func selector test

--- mfunc_test.go
+++ mfunc_test.go
@@ -7,18 +7,30 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("funcSelector", func() {
-	Context("newFuncSelector", func() {
-		It("has the correct mergefuncs", func() {
-			fs := newFuncSelector()
+var _ = Describe("newFuncSelector", func() {
+	var (
+		fs *funcSelector
+	)
 
-			mapMerge, mapOk := fs.typeFuncs[reflect.TypeOf(map[string]interface{}{})]
-			Expect(mapOk).To(BeTrue())
-			Expect(mapMerge).ToNot(BeNil())
+	BeforeEach(func() {
+		fs = newFuncSelector()
+	})
 
-			sliceMerge, sliceOK := fs.typeFuncs[reflect.TypeOf([]interface{}{})]
-			Expect(sliceOK).To(BeTrue())
-			Expect(sliceMerge).ToNot(BeNil())
-		})
+	It("has the correct type mergefuncs", func() {
+		mapMerge, mapOk := fs.typeFuncs[reflect.TypeOf(map[string]interface{}{})]
+		Expect(mapOk).To(BeTrue())
+		Expect(mapMerge).ToNot(BeNil())
+
+		sliceMerge, sliceOK := fs.typeFuncs[reflect.TypeOf([]interface{}{})]
+		Expect(sliceOK).To(BeTrue())
+		Expect(sliceMerge).ToNot(BeNil())
+	})
+
+	It("has kind mergeFuncs map", func() {
+		Expect(fs.kindFuncs).ToNot(BeNil())
+	})
+
+	It("has default mergeFunc", func() {
+		Expect(fs.defaultFunc).ToNot(BeNil())
 	})
 })
